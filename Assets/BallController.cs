60759dde348a0a15ab6421cdc9f8f46a63c89f87
RDArtemis <144180579+RDArtemis@users.noreply.github.com>
2025-02-03 19:54:54 -0800

    added isBallLaunched

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -6,8 +6,8 @@ public class BallController : MonoBehaviour
 
 {
     [SerializeField] private float force = 1f;
-    [SerializeField] private InputManager inputManager;
-
+    private InputManager inputManager;
+    private bool isBallLaunched;
     private Rigidbody  ballRB;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -22,6 +22,8 @@ public class BallController : MonoBehaviour
 
     private void LaunchBall()
     {
+        if (isBallLaunched) return;
+        isBallLaunched = true;
         ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
     }
 
